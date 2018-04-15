73bd5872ed3587b05b4ab8640c3f88e3e8560730
KristijanPetrov <33343368+KristijanPetrov@users.noreply.github.com>
2018-04-15 17:59:56 +0300

    Rename hw1oop.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿// hw1oop.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+#include "Planet.h"
+#include "Jedi.h"
+#include "Stormtrooper.h"
+#include "JediTemple.h"
+#include "Army.h"
+#include "PlanetSystem.h"
+#include "Galaxy.h"
+#include "GalacticRepublic.h"
+#include "RepublicList.h"
+
+int main()
+{
+	
+
+	system("PAUSE");
+    return 0;
+}
+
